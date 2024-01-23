f1516ad9c804e42a925f84e56d46b81d77cc8b5f
emreylmz7 <yilmazvemre@gmail.com>
2024-01-23 22:50:24 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Domain.Enums;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; } = null!;
+        public string? Model { get; set; }
+        public string? CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public Transmission TransmissionType { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public FuelType Fuel { get; set; }
+        public string? MainImageUrl { get; set; }
+        public List<CarFeature>? CarFeatures { get; set; }
+        public List<CarDescription>? CarDescriptions { get; set; }
+        public List<CarPricing>? CarPricing { get; set; }
+    }
+}
