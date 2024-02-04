27bd707d3e43cc880e942a27b9d6b94beaa0fb9c
emreylmz7 <yilmazvemre@gmail.com>
2024-02-04 23:38:58 +0300

    view components added

---
+++ Frontend/CarBookProject.WebUI/ViewComponents/UILayout/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.UILayout
+{
+	public class _MainCoverUILayoutComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
